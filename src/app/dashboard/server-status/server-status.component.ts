7629124bf044433e7a89623ae8ccba8a62c34e1d
Vyahruth07 <vyahruthnetcherla@gmail.com>
2024-07-23 19:34:57 +0530

    manipulating state and using literal values

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DashboardItemComponent } from "../dashboard-item/dashboard-item.component";
 
 @Component({
@@ -6,8 +6,22 @@ import { DashboardItemComponent } from "../dashboard-item/dashboard-item.compone
   standalone: true,
   imports: [DashboardItemComponent],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit,OnDestroy {
+  currentStatus: 'offline' | 'online' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>
+  constructor(){
+  }
+  ngOnInit(){
+    setInterval(() => {
+      const rnd=Math.random();
+      if(rnd<0.5) this.currentStatus='online';
+      else if(rnd<0.9) this.currentStatus='offline';
+      else this.currentStatus='unknown';
+    },2500);
+  }
+  ngOnDestroy(){
+    clearTimeout(this.interval);
+  }
 }
